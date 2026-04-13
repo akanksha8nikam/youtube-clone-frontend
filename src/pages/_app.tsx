5050353356500a1ec451d3978c216c64610d6bfc
Akanksha Nikam <akanksha8.nikam@gmail.com>
2026-04-13 22:19:39 +0530

    fix: remove hardcoded background from app wrapper to fix style clash

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,7 +7,7 @@ import { UserProvider } from "../lib/AuthContext";
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <UserProvider>
-      <div className="min-h-screen bg-white text-black">
+      <div className="min-h-screen bg-transparent">
         <title>Your-Tube Clone</title>
         <Header />
         <Toaster />
